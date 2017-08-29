70f5cbb45f54c93588b16025bbcde5edf829b7c9
root <root@localhost.localdomain>
2017-08-29 10:47:30 +0800

    上传

---
+++ wea/conky.py
@@ -0,0 +1,62 @@
+#coding=utf-8
+import urllib
+import re
+import os
+import time
+def getWeather(html,findkey):
+    reg = findkey
+    weatherList = re.compile(reg).findall(html)
+    return weatherList
+def getHtml(url):
+    page = urllib.urlopen(url)
+    html = page.read()
+    page.close()
+    return html
+def changeIcon(icon) :
+    if icon == '晴' :
+        dt=list(time.localtime())
+        if (dt[3] >= 19) or (dt[3] <= 6) :
+            os.system('rm -rf /wea/显示/1.png')  
+            os.system('cp /wea/图片/Moon.png /wea/显示/1.png')
+        else :
+            os.system('rm -rf /wea/显示/1.png')  
+            os.system('cp /wea/图片/Sun.png /wea/显示/1.png')
+    if icon == '多云' :
+        dt=list(time.localtime())
+        if (dt[3] >= 19) or (dt[3] <= 6) :
+            os.system('rm -rf /wea/显示/1.png')  
+            os.system('cp /wea/图片/Cloudy-Nighttime.png /wea/显示/1.png')
+        else :
+            os.system('rm -rf /wea/显示/1.png')  
+            os.system('cp /wea/图片/Cloudy-Daytime.png /wea/显示/1.png')
+    if icon == '阴' :
+        dt=list(time.localtime())
+        os.system('rm -rf /wea/显示/1.png')  
+        os.system('cp /wea/图片/Overcast-Sky.png /wea/显示/1.png')
+    if icon == '雷阵雨' :
+        dt=list(time.localtime())
+        os.system('rm -rf /wea/显示/1.png')  
+        os.system('cp /wea/图片/Thunder-Shower.png /wea/显示/1.png')
+    if (icon == '小雨') or (icon == '中雨') or (icon == '大雨') or (icon == '暴雨') or (icon == '阵雨') :
+        dt=list(time.localtime())
+        os.system('rm -rf /wea/显示/1.png')  
+        os.system('cp /wea/图片/Drizzle.png /wea/显示/1.png')
+    if (icon == '小雪') or (icon == '中雪') or (icon == '大雪') or (icon == '暴雪') or (icon == '阵雪') :
+        dt=list(time.localtime())
+        os.system('rm -rf /wea/显示/1.png')  
+        os.system('cp /wea/图片/Snow.png /wea/显示/1.png')
+try :
+    m =getHtml('http://www.weather.com.cn/weather/101190402.shtml')
+    high = getWeather(m,'<span>(.*?)</span>/<i>')
+    low = getWeather(m,'</span>/<i>(.*?)</i>')
+    condition = getWeather(m,'class="wea">(.*?)</p>')
+    print condition[0]
+    print '          ' + high[0] +'～' + low[0]
+    t = condition[0] +'n'
+    img = getWeather(t,'转(.*?)n')
+    if img == []:
+        changeIcon(condition[0])
+    else :
+        changeIcon(img[0])
+except :
+    print 'ERROR!'
